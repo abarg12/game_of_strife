e3ad055c775dbfbee06859c672d137879da9c04c
abarg12 <aidan.barg@gmail.com>
2022-10-12 12:19:16 -0400

    grid pattern drawn

--- game.py
+++ game.py
@@ -12,14 +12,13 @@ size = width, height = UNIVERSE_SIZE*10, UNIVERSE_SIZE*10
 screen = pygame.display.set_mode(size)
 
 # Genesis
-universe = [[0]*UNIVERSE_SIZE]*UNIVERSE_SIZE
+universe = [[0]*UNIVERSE_SIZE for i in range(UNIVERSE_SIZE)]
 print(universe)
 
-#for i in range(0, UNIVERSE_SIZE):
-#    for j in range(0, UNIVERSE_SIZE):
-#        if ((i % 2) == 0):
-#            universe[i][j] = 1
-universe[2][2] = 1
+for i in range(UNIVERSE_SIZE):
+    for j in range(UNIVERSE_SIZE):
+        if (((i+j) % 2) == 0):
+            universe[i][j] = 1
 print(universe)
 
     
@@ -28,8 +27,8 @@ while 1:
         if event.type == pygame.QUIT: sys.exit()
     
     screen.fill(black)
-    for i in range(0, UNIVERSE_SIZE):
-        for j in range(0, UNIVERSE_SIZE):
+    for i in range(UNIVERSE_SIZE):
+        for j in range(UNIVERSE_SIZE):
             if (universe[i][j] == 1):
                 rectangle1 = pygame.Rect(i*10, j*10, 10, 10)
                 pygame.draw.rect(screen, white, rectangle1)
